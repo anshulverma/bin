bbccf222acaeae49a7d9a8409e0d3d61f4f42680
Anshul Verma <anshul.verma86@gmail.com>
2015-07-13 13:27:53 -0700

    add spotify apple script

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 BASEDIR=$(dirname $0)
 echo "export PATH=\$PATH:$BASEDIR/scripts  # add custom scripts" >> ~/.bashrc
+echo "export PATH=\$PATH:$BASEDIR/applescripts  # add custom apple scripts" >> ~/.bashrc
 echo "success"
